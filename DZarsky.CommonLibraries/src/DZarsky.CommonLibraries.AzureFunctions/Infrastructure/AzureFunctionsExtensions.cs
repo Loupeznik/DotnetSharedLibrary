f6fbd883eaf6b4bf9e2e3c257a1ff6fa93cabde5
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-08-27 17:47:40 +0200

    fix missing using

--- DZarsky.CommonLibraries/src/DZarsky.CommonLibraries.AzureFunctions/Infrastructure/AzureFunctionsExtensions.cs
+++ DZarsky.CommonLibraries/src/DZarsky.CommonLibraries.AzureFunctions/Infrastructure/AzureFunctionsExtensions.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Zitadel.Credentials;
 using Zitadel.Extensions;
 
 namespace DZarsky.CommonLibraries.AzureFunctions.Infrastructure
